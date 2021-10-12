2d5daae40259baf76b1d870664e3fcc04f35be9b
Koko-66 <k.cisek@gmail.com>
2021-10-12 08:25:38 +0000

    Basic setup

--- run.py
+++ run.py
@@ -1,16 +1,24 @@
-from flask import Flask # import a class Flask from flask
+from flask import Flask, render_template  # import a class Flask from flask
 import os
 
-app = Flask(__name__) # create an instance of Flask class and 
-                     # store it in app variable (customary)
+# create an instance of Flask class and store it in app variable (customary)
+app = Flask(__name__)
+
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return render_template("index.html")
 
 
-if __name__ == "__main__": #__main__ is python main module
+@app.route("/about")
+def index():
+    return render_template("about.html")
+
+
+if __name__ == "__main__":  # __main__ is python main module
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
-        debug=True) #DO NOT set to True when in production or when submitting the project as it leads to vulnerabilities!
+        debug=True)
+# DO NOT set to True when in production or when submitting the project as it
+# leads to vulnerabilities!
