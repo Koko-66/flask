a85d916b3ed3a15430429656194723a5c429fe55
Koko-66 <k.cisek@gmail.com>
2021-10-13 23:11:11 +0000

    Add external data and loops in About file.

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
+import json
 import os
 from flask import Flask, render_template  # import a class Flask from flask
-
 
 # create an instance of Flask class and store it in app variable (customary)
 app = Flask(__name__)
@@ -13,17 +13,21 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About",
+                           company=data)
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Career")
 
 
 if __name__ == "__main__":  # __main__ is python main module
